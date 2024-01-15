de05c7bc5a1dfc0458aef8cc26244ded4cca9760
impROS <impROS@hotmail.com>
2024-01-15 19:47:16 +0300

    remove : unused imports

--- src/main/java/com/example/_06_custom_filter_02/WebConfig.java
+++ src/main/java/com/example/_06_custom_filter_02/WebConfig.java
@@ -3,13 +3,6 @@ package com.example._06_custom_filter_02;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.filter.OncePerRequestFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 
 @Configuration
 public class WebConfig {
